929932928721c40a013a15fbdc664ff8a5c667af
KATESMETHADDICTION <rustprosigma@outlook.com>
2025-07-05 22:07:39 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import FeaturedBuilds from "@/components/FeaturedBuilds";
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <FeaturedBuilds />
+      <About />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default Index;
