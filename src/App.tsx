85f8349dc8603e2b4e7550a6b4f0e36d66bd2ca3
KATESMETHADDICTION <rustprosigma@outlook.com>
2025-07-05 22:18:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LiveChatWidget from "./components/LiveChatWidget";
 import StaffDashboard from "./pages/StaffDashboard";
 import Legal from "./pages/Legal";
-// ...other imports
+import Index from "./pages/Index"; // <-- Import your homepage
 
 function App() {
   return (
     <BrowserRouter>
       <LiveChatWidget />
       <Routes>
-        {/* ...other routes */}
+        <Route path="/" element={<Index />} /> {/* Home route */}
         <Route path="/staff" element={<StaffDashboard />} />
         <Route path="/legal" element={<Legal />} />
       </Routes>
